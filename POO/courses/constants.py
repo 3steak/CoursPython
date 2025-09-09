0121ded4d099a8b1e91926d48f4bc1b5397bd1c0
3steak <cyprien.bocquet@gmail.com>
2025-09-09 10:56:55 +0200

    ajout methode sauvegarder || ecriture fichier json

--- POO/courses/constants.py
+++ POO/courses/constants.py
@@ -1,4 +1,6 @@
 import os
+import logging
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(CUR_DIR, "data")
+LOGGER = logging.getLogger()
--- POO/courses/lib.py
+++ POO/courses/lib.py
@@ -1,8 +1,10 @@
-import logging
-LOGGER = logging.getLogger()
+import json
+import os
+
+from constants import DATA_DIR, LOGGER
 
 class Liste(list):
-    # list pour heriter des methodes de list comme append etc
+    # "list" pour heriter des methodes de list comme append etc
     def ajouter(self, element):
         if not isinstance(element, str):
             raise ValueError("La liste doit contenir que des mots.")
@@ -19,9 +21,31 @@ class Liste(list):
             raise ValueError("L'element n'est pas dans la liste")
         self.remove(element)
         return True
-    
+
+    def afficher(self):
+        print(f"Ma liste de {self.nom} :")
+        for element in self:
+            print(f"- {element}")
+            
+    def sauvegarder(self):
+        chemin = os.path.join(DATA_DIR, f"{self.nom}.json")
+        if not os.path.exists(DATA_DIR):
+            os.makedirs(DATA_DIR)
+        
+        with open(chemin, "w") as f:
+            json.dump(self, f, indent=4)
+
+        return True
+
     def __init__(self, nom):
         self.nom = nom
 
 if __name__ == "__main__":
     print("Hello")
+
+liste = Liste("courses")
+liste.ajouter("Pommes")
+liste.ajouter("Poires")
+liste.ajouter("Peches")
+liste.afficher()
+liste.sauvegarder()
